251e628d9ee146d19c51006a80c4e67841402fd5
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-03-16 17:41:25 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ radio.set_group(250)
 RunComp.set_light_level()
 
 cas = 0
-zacatek = 0
-konec = 0
+cas1 = 0
+cas2 = 0
 
 def on_light_drop():
-    global cas
-    finish = control.millis()
-    cas = zacatek - konec
+    cas2 = control.millis()
     music.play_tone(Note.A, 1000)
+    print(cas)
 RunComp.on_light_drop(on_light_drop)
-print(cas)
+
 
 def on_received_number(number):
-    start = control.millis()
+    global cas1
+    cas1 = control.millis()
 radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 radio.setGroup(250)
 RunComp.SetLightLevel()
 let cas = 0
-let zacatek = 0
-let konec = 0
+let cas1 = 0
+let cas2 = 0
 RunComp.OnLightDrop(function on_light_drop() {
-    
-    let finish = control.millis()
-    cas = zacatek - konec
+    let cas2 = control.millis()
     music.playTone(Note.A, 1000)
+    console.log(cas)
 })
-console.log(cas)
 radio.onReceivedNumber(function on_received_number(number: number) {
-    let start = control.millis()
+    
+    cas1 = control.millis()
 })
